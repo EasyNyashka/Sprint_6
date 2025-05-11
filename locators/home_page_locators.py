c7c57d5a7d25418da0707431f90e33d223b85ae2
Elena Ermolenkova <e-wolf@rambler.ru>
2025-05-11 17:21:59 +0300

    Создание файла order_hage_locators.py и внесение в него локаторов заполнения формы заказа.

--- locators/home_page_locators.py
+++ locators/home_page_locators.py
@@ -11,12 +11,12 @@ class HomePageLocators:
     # Кнопка заказа верхняя
     ORDER_BUTTON_TOP = (By.XPATH, "//div[@class='Header_Nav__AGCXC'] and //button[contains(@class, 'Button_Button__ra12g')")
 
-    #Кнопка заказа нижняя
+    # Кнопка заказа нижняя
     ORDER_BUTTON_LOWER = (By.XPATH, "//button[@class='Button_Middle__1CSJM']")
 
     # Вопрос
     QUESTION = (By.ID, 'accordion__heading-{}')
-    
+
     # Ответ
     ANSWER = (By.ID, 'accordion__panel-{}')
 
---
+++ locators/order_page_locators.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+
+class OrderPageLocators:
+
+    # Поле ввода Имя
+    NAME = (By.XPATH, "//input[@placeholder='* Имя']")
+    # Поле ввода Фамилии
+    SURNAME = (By.XPATH, "//input[@placeholder='* Фамилия']")
+    # Поле ввода адреса
+    ADRESS = (By.XPATH, "//input[@placeholder='* Адрес: куда привезти заказ']")
+    # поле ввода станции метро
+    METRO_STATION = (By.XPATH, "//input[@class='select-search__input']")
+    # Поле ввода телефона
+    TELEPHONE = (By.XPATH, "//input[@placeholder='* Телефон: на него позвонит курьер']")
+    # Кнопка Далее
+    BUTTON_NEXT = (By.XPATH, "//button[text()='Далее']")
+    # Поле ввода Когда привезти самокат
+    WHEN_TO_BRING = (By.XPATH, "//input[@placeholder='* Когда привезти самокат']")
+    # Поле ввода Срок аренды
+    RENTAL_PERIOD = (By.XPATH, "//div[@class='Dropdown-root is-open']")
+    # Выпадающий список
+    NUMBER_OF_DAYS = (By.XPATH, "//div[@class='Dropdown - placeholder is -selected']")
+    # Поле ввода Цвет самоката
+    SCOOTER_COLOR = (By.XPATH, "//input[@type='checkbox' and text{}]")
+    # Поле ввода Комментарий
+    COMMENT = (By.XPATH, "//input[@placeholder='Комментарий для курьера']")
+    # Кнопка Заказать
+    BUTTON_ORDER = (By.XPATH, "//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Заказать']")
+
+
