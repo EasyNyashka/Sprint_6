b67963ceac6d83f856f396a28d0bb8bcd88bc123
Elena Ermolenkova <e-wolf@rambler.ru>
2025-05-11 15:18:42 +0300

    Создание файла home_hage_locators.py и внесение в него локаторов на логотип Яндекс Самокат, верхнюю и нижнюю кнопки Заказать и на блок Вопрос-ответ.

---
+++ locators/home_page_locators.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+
+class HomePageLocators:
+
+    # Логотип Яндекс
+    LOGO_YANDEX = (By.CLASS_NAME, "Header_LogoYandex__3TSOI")
+
+    # Логотип Самокат
+    LOGO_SCOOTER = (By.CLASS_NAME, "Header_LogoScooter__3lsAR")
+
+    # Кнопка заказа верхняя
+    ORDER_BUTTON_TOP = (By.XPATH, "//div[@class='Header_Nav__AGCXC'] and //button[contains(@class, 'Button_Button__ra12g')")
+
+    #Кнопка заказа нижняя
+    ORDER_BUTTON_LOWER = (By.XPATH, "//button[@class='Button_Middle__1CSJM']")
+
+    # Вопрос
+    QUESTION = (By.ID, 'accordion__heading-{}')
+    
+    # Ответ
+    ANSWER = (By.ID, 'accordion__panel-{}')
+
